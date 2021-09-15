ae57b0771abb8fb988ffaddb09c3640520ecbbd8
Erdnando Rodriguez Vargas <erdnando@gmail.com>
2021-09-15 11:21:16 -0500

    remove comments

--- DigitalData.DDoc.Common/Entities/Helpers/CollectionChangeRequest.cs
+++ DigitalData.DDoc.Common/Entities/Helpers/CollectionChangeRequest.cs
@@ -1,9 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: DigitalData.DDoc.Common.Entities.Helpers.CollectionChangeRequest
-// Assembly: DigitalData.DDoc.Common, Version=4.210.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 37D7F3E1-2CA5-48B0-AB22-FE81D67009C0
-// Assembly location: C:\Users\herna\Downloads\ss\BradesCard_DEV\bin\DigitalData.DDoc.Common.dll
-
+﻿
 using System.Collections.Generic;
 
 namespace DigitalData.Open.Common.Entities.Helpers
--- DigitalData.DDoc.Common/Extensions/1IFolderPreProcessor.cs
+++ DigitalData.DDoc.Common/Extensions/1IFolderPreProcessor.cs
@@ -1,8 +1,4 @@
 ﻿// Decompiled with JetBrains decompiler
-// Type: DigitalData.DDoc.Common.Extensions.IFolderPreProcessor
-// Assembly: DigitalData.DDoc.Common, Version=4.210.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 37D7F3E1-2CA5-48B0-AB22-FE81D67009C0
-// Assembly location: C:\Users\herna\Downloads\ss\BradesCard_DEV\bin\DigitalData.DDoc.Common.dll
 
 using DigitalData.Open.Common.Entities;
 using System;
